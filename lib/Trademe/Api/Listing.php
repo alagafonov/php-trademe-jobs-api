29554352b63b1e21cfbe8230c993192991b7debf
Alex Agafonov <alexsdn@gmail.com>
2016-11-18 10:19:24 +1000

    Completing listing end point.

--- lib/Trademe/Api/Listing.php
+++ lib/Trademe/Api/Listing.php
@@ -4,7 +4,7 @@ use Trademe\Entities\Listing as ListingEntity;
 use Trademe\Factories\ListingFactory;
 
 /**
- * Locality end point.
+ * Listing end point
  */
 class Listing extends ApiAbstract
 {
@@ -19,38 +19,98 @@ class Listing extends ApiAbstract
         );
     }
 
-    public function create(array $data)
+    /**
+     * @param ListingEntity $listing
+     * @return mixed
+     */
+    public function create(ListingEntity $listing)
     {
-        return $this->post('/Selling.json', $data);
+        return $this->post('/Selling.json', $listing->getArray());
     }
 
-    public function update(array $data)
+    /**
+     * @param ListingEntity $listing
+     * @return mixed
+     */
+    public function update(ListingEntity $listing)
     {
-        return $this->post('/Selling/Edit.json', $data);
+        return $this->post('/Selling/Edit.json', $listing->getArray());
     }
 
+    /**
+     * @param ListingEntity $listing
+     * @return mixed
+     */
     public function validate(ListingEntity $listing)
     {
         return $this->post('/Selling/Validate.json', $listing->getArray());
     }
 
-    public function relist(array $data)
+    /**
+     * @param int $listingId
+     * @return mixed
+     */
+    public function relist($listingId)
     {
-        return $this->post('/Selling/Relist.json', $data);
+        return $this->post(
+            '/Selling/Relist.json',
+            [
+                'ListingId'            => $listingId,
+                'ReturnListingDetails' => false,
+            ]
+        );
     }
 
+    /**
+     * Note: Currently, this can use a similar payload to edit, what response of the Retrieve a job listing endpoint.
+     * However, this is not guaranteed in the future.
+     *
+     * @param ListingEntity $listing
+     * @return mixed
+     */
     public function relistWithEdits(ListingEntity $listing)
     {
         return $this->post('/Selling/RelistWithEdits.json', $listing->getArray());
     }
 
-    public function duplicate(array $data)
+    /**
+     * @param int $listingId
+     * @return mixed
+     */
+    public function duplicate($listingId)
     {
-        return $this->post('/Selling/Similar.json', $data);
+        return $this->post(
+            '/Selling/Similar.json',
+            [
+                'ListingId'            => $listingId,
+                'ReturnListingDetails' => false,
+            ]
+        );
     }
 
-    public function withdraw(array $data)
+    /**
+     * @param int $listingId
+     * @param boolean $positionFilled
+     * @return mixed
+     */
+    public function withdraw($listingId, $positionFilled)
     {
-        return $this->post('/Selling/Withdraw.json', $data);
+        if ($positionFilled) {
+            $type = 'ListingWasSold';
+            $reason = 'Position filled';
+        } else {
+            $type = 'ListingWasNotSold';
+            $reason = 'Position was not filled';
+        }
+
+        return $this->post(
+            '/Selling/Withdraw.json',
+            [
+                'ListingId'            => $listingId,
+                'Type'                 => $type,
+                'Reason'               => $reason,
+                'ReturnListingDetails' => false,
+            ]
+        );
     }
 }
